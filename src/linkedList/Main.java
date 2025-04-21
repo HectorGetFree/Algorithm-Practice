62c513914d1f6439c482b11c049e5bfbfb2d922e
Hector <hectorgetfree@gmail.com>
2025-04-21 21:54:55 +0800

    leetcode 876

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -2,18 +2,15 @@ package linkedList;
 
 import static linkedList.leetcode_21.S_1.mergeTwoLists;
 import static linkedList.leetcode_82.S_1.deleteDuplicates;
+import static linkedList.leetcode_876.S_1.middleNode;
 
 public class Main {
     public static void main(String[] args) {
-        ListNode l13 = new ListNode(4, null);
-        ListNode l12 = new ListNode(2, l13);
-        ListNode list1 = new ListNode(1, l12);
-
-        ListNode l23 = new ListNode(4, null);
-        ListNode l22 = new ListNode(3, l23);
-        ListNode list2 = new ListNode(1, l22);
-
-
-        mergeTwoLists(list1, list2);
+        ListNode l5 = new ListNode(5, null);
+        ListNode l4 = new ListNode(4, l5);
+        ListNode l3 = new ListNode(3, l4);
+        ListNode l2 = new ListNode(2, l3);
+        ListNode l1 = new ListNode(1, l2);
+        middleNode(l1);
     }
 }
---
+++ src/linkedList/leetcode_876/S_1.java
@@ -0,0 +1,31 @@
+package linkedList.leetcode_876;
+
+import linkedList.ListNode;
+
+public class S_1 {
+    public static ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        int counter = 0;
+        ListNode p = head;
+        while (p != null) {
+            counter++;
+            p = p.next;
+        }
+
+        int mid;
+        if (counter % 2 == 0) {
+            mid = (counter / 2) + 1;
+        } else {
+            mid = (counter + 1) / 2;
+        }
+        counter = 0;
+        p = head;
+        while (counter < mid) {
+            counter++;
+            p = p.next;
+        }
+        return p;
+    }
+}
